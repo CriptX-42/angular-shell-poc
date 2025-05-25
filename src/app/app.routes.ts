fe73a361f5218967a61fa3db7e3b3319b3e60017
Ricardo Carvalho <ricardo-svc@live.com>
2025-05-24 21:53:38 -0300

    Criando host para MFE no angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'remote',
+    loadChildren: () =>
+      loadRemoteModule({
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        remoteName: 'mfe1',
+        exposedModule: './ProfileModule',
+      }).then((m) => m.ProfileModule),
+  },
+];
